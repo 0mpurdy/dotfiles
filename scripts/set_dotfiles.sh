3b5ebec860d603f3b98131c9846bd0e939eb5c04
Michael Purdy <0mpurdy@gmail.com>
2018-10-05 23:40:52 +0100

    Use better pandoc css location

--- scripts/set_dotfiles.sh
+++ scripts/set_dotfiles.sh
@@ -1,6 +1,6 @@
 dotpath="${HOME}/dev/dotfiles"
 
 cp "${dotpath}/nix/.bashrc" "${HOME}/.bashrc"
-cp "${dotpath}/pandoc/panread.css" "${HOME}/pan.css"
+cp "${dotpath}/pandoc/panread.css" "${HOME}/.config/pan.css"
 cp "$dotpath/editor/.vimrc" "${HOME}/.vimrc"
 cp "${dotpath}/editor/nvim/init.vim" "${HOME}/.config/nvim/init.vim"
