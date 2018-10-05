ba10a81a355cbac33ef62c64a82fafb23edec7dc
Michael Purdy <0mpurdy@gmail.com>
2018-10-05 23:43:13 +0100

    Add script for setting up git configuration

---
+++ scripts/setup-git.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Michael Purdy"
+git config --global user.email "0mpurdy@gmail.com"
+git config --global --add alias.lola 'log --graph --decorate --abbrev-commit --all --date=local --pretty=format:"%C(auto)%h%d %C(blue)%an %C(green)%cd %C(red)%GG %C(reset)%s" --date=local'
+git config --global --add alias.lol 'log --graph --decorate --abbrev-commit --all --oneline'
+
