cf56c5b72b3183695ce7cfbeedd595e938201545
Michael Purdy <0mpurdy@gmail.com>
2018-10-05 22:27:59 +0100

    Update set dotfiles script

--- scripts/set_dotfiles.sh
+++ scripts/set_dotfiles.sh
@@ -1,10 +1,6 @@
 dotpath="${HOME}/dev/dotfiles"
 
-function overwrite_file {
-  rm -rf $2
-  cp $1 $2
-}
-
-overwrite_file "${dotpath}/pandoc/panread.css" "${HOME}/pan.css"
-overwrite_file "$dotpath/editor/.vimrc" "${HOME}/.vimrc"
-overwrite_file "${dotpath}/editor/nvim/init.vim" "${HOME}/.config/nvim/init.vim"
+cp "${dotpath}/nix/.bashrc" "${HOME}/.bashrc"
+cp "${dotpath}/pandoc/panread.css" "${HOME}/pan.css"
+cp "$dotpath/editor/.vimrc" "${HOME}/.vimrc"
+cp "${dotpath}/editor/nvim/init.vim" "${HOME}/.config/nvim/init.vim"
