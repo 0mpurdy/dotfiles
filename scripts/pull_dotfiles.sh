a05438db7e3a7751c81748bed54256f9dc99c3b5
Michael Purdy <0mpurdy@gmail.com>
2018-10-12 20:20:38 +0100

    Sync push and pull of dotfiles

--- scripts/pull_dotfiles.sh
+++ scripts/pull_dotfiles.sh
@@ -1,6 +1,7 @@
 dotpath="${HOME}/dev/dotfiles"
 
-cp "${HOME}/.bashrc" "${dotpath}/nix"
-cp "${HOME}/.vimrc" "${dotpath}/editor"
-cp "${HOME}/.profile" "${dotpath}/nix"
-cp "${HOME}/.config/nvim/init.vim" "${dotpath}/editor/nvim"
+cp "${HOME}/.bashrc" "${dotpath}/nix/.bashrc"
+cp "${dotpath}/pandoc/panread.css" "${HOME}/.config/pan.css"
+cp "${HOME}/.vimrc" "${dotpath}/editor/.vimrc"
+cp "${HOME}/.profile" "${dotpath}/nix/.profile"
+cp "${HOME}/.config/nvim/init.vim" "${dotpath}/editor/nvim/init.vim"
