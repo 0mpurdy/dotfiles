b221ae1cc1327134926cecaf1f9b7440e7f6916d
Michael Purdy <0mpurdy@gmail.com>
2020-12-16 17:17:35 +0000

    Add windows vimrc
    Compatible with Visual Studio vim plugin

--- win/pull_dotfiles.ps1
+++ win/pull_dotfiles.ps1
@@ -1,4 +1,5 @@
 param([string] $username = $env:USERNAME)
 echo "Pulling dotfiles for $username"
-cp "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1" "C:/dev/dotfiles/win/profile.ps1"
-cp "$env:LOCALAPPDATA/nvim/init.vim" "C:/dev/dotfiles/editor/nvim/init.vim"
+cp "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1" "T:/dev/dotfiles/win/profile.ps1"
+cp "$env:LOCALAPPDATA/nvim/init.vim" "T:/dev/dotfiles/editor/nvim/init.vim"
+cp "C:/Users/$username/_vimrc" "T:/dev/dotfiles/win/_vimrc"
--- win/push_dotfiles.ps1
+++ win/push_dotfiles.ps1
@@ -1,3 +1,6 @@
 param([string] $username = $env:USERNAME)
-cp C:/dev/dotfiles/win/profile.ps1 "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1"
-cp C:/dev/dotfiles/editor/nvim/init.vim "$env:LOCALAPPDATA/nvim/init.vim"
+New-Item -ItemType directory -Path "C:/Users/$username/Documents/WindowsPowerShell" -Force
+cp "T:/dev/dotfiles/win/profile.ps1" "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1"
+New-Item -ItemType directory -Path "$env:LOCALAPPDATA/nvim" -Force
+cp "T:/dev/dotfiles/editor/nvim/init.vim" "$env:LOCALAPPDATA/nvim/init.vim"
+cp "T:/dev/dotfiles/win/_vimrc" "C:/Users/$username/_vimrc"
