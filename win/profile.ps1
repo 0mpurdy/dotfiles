5a8f43791b013d2587e363201cce5f5b5bde172c
Michael Purdy <0mpurdy@gmail.com>
2021-02-07 00:57:56 +0000

    Update profile methods to use profile ENV var

--- win/profile.ps1
+++ win/profile.ps1
@@ -1,10 +1,18 @@
 param([string] $username = $env:USERNAME)
 
-Write-Host ('Profile for', $username, 'in "Documents/WindowsPowerShell"') -Separator ' '
+Write-Host ('----')
+Write-Host ('$PSCommandPath', $PSCommandPath)
+Write-Host ('$Profile', $profile)
 
-function go {cd C:\dev}
-function goconfig {cd "C:\Users\$username\Documents\WindowsPowerShell"}
-function ecfg {nvim "C:\Users\$username\Documents\WindowsPowerShell\profile.ps1"}
+function go {cd "C:\dev"}
+function work {cd "T:\dev\work"}
 
-# to apply changes to the current powershell session use .
+$profileDirectory = $profile | Split-Path
+function goconfig {cd $profileDirectory}
+function ecfg {nvim $profile}
+
+function editOldConfig {nvim "C:\Users\$username\Documents\WindowsPowerShell\profile.ps1"}
+function resetOldConfig {. "C:\Users\$username\Documents\WindowsPowerShell\profile.ps1"}
+
+# to apply changes to the current powershell session use . or resetCfg
 # eg: `. ./profile.ps1`
