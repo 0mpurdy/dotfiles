f78bd7b4a5c61daf4d5fcbea7a7b6dfe861bcb1e
Michael Purdy <0mpurdy@gmail.com>
2021-07-01 16:36:46 +0100

    Add vsvimrc

--- win/pull_dotfiles.ps1
+++ win/pull_dotfiles.ps1
@@ -1,5 +1,11 @@
 param([string] $username = $env:USERNAME)
+
 echo "Pulling dotfiles for $username"
-cp "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1" "T:/dev/dotfiles/win/profile.ps1"
-cp "$env:LOCALAPPDATA/nvim/init.vim" "T:/dev/dotfiles/editor/nvim/init.vim"
-cp "C:/Users/$username/_vimrc" "T:/dev/dotfiles/win/_vimrc"
+
+$dotfilesLoc='T:/dev/dotfiles'
+
+cp "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1" "$dotfilesLoc/win/legacy-profile.ps1"
+cp "$profile" "$dotfilesLoc/win/profile.ps1"
+cp "$env:LOCALAPPDATA/nvim/init.vim" "$dotfilesLoc/editor/nvim/init.vim"
+cp "C:/Users/$username/_vimrc" "$dotfilesLoc/win/_vimrc"
+cp "C:/Users/$username/_vsvimrc" "$dotfilesLoc/win/_vsvimrc"
--- win/push_dotfiles.ps1
+++ win/push_dotfiles.ps1
@@ -1,6 +1,10 @@
 param([string] $username = $env:USERNAME)
+
+$dotfilesLoc='T:/dev/dotfiles'
+
 New-Item -ItemType directory -Path "C:/Users/$username/Documents/WindowsPowerShell" -Force
-cp "T:/dev/dotfiles/win/profile.ps1" "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1"
+cp "$dotfilesLoc/win/profile.ps1" "$profile"
 New-Item -ItemType directory -Path "$env:LOCALAPPDATA/nvim" -Force
-cp "T:/dev/dotfiles/editor/nvim/init.vim" "$env:LOCALAPPDATA/nvim/init.vim"
-cp "T:/dev/dotfiles/win/_vimrc" "C:/Users/$username/_vimrc"
+cp "$dotfilesLoc/editor/nvim/init.vim" "$env:LOCALAPPDATA/nvim/init.vim"
+cp "$dotfilesLoc/win/_vimrc" "C:/Users/$username/_vimrc"
+cp "$dotfilesLoc/win/_vsvimrc" "C:/Users/$username/_vsvimrc"
