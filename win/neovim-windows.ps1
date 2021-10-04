f4f24ead1369727e645744b3f078c25cae15bae7
Michael Purdy <0mpurdy@gmail.com>
2021-10-04 23:19:13 +0100

    Add neovim path note for windows

--- win/neovim-windows.ps1
+++ win/neovim-windows.ps1
@@ -8,3 +8,5 @@ python -m pip install pynvim
 # https://github.com/junegunn/vim-plug
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$env:LOCALAPPDATA/nvim-data/site/autoload/plug.vim" -Force
+
+Write-Output "`n`n`n`nYou may need to add 'C:\tools\neovim\Neovim\bin' to the Path"
