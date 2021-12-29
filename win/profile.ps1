3772a53e837f31edac5e6fbcf3e4f03963178a57
Michael Purdy <0mpurdy@gmail.com>
2021-12-29 11:48:18 +0000

    Add quick cd aliases

--- win/profile.ps1
+++ win/profile.ps1
@@ -1,3 +1,5 @@
+# to apply changes to the current powershell session use `. $profile`
+
 param([string] $username = $env:USERNAME)
 
 Write-Host ('----')
@@ -5,6 +7,9 @@ Write-Host ('$PSCommandPath', $PSCommandPath)
 Write-Host ('$Profile', $profile)
 
 function go {cd "C:\dev"}
+function dev {cd "C:\dev"}
+function notes {cd "C:\dev\notes"}
+function tools {cd "C:\dev\tools"}
 function work {cd "T:\dev\work"}
 
 $profileDirectory = $profile | Split-Path
