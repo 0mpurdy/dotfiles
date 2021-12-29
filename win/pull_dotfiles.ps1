ea5575f8faabb97db155fa5a5aaca51b02b63d7f
Michael Purdy <0mpurdy@gmail.com>
2021-12-29 12:00:36 +0000

    Ignore error for missing legacy windows profile

--- win/pull_dotfiles.ps1
+++ win/pull_dotfiles.ps1
@@ -4,7 +4,7 @@ echo "Pulling dotfiles for $username"
 
 $dotfilesLoc='T:/dev/dotfiles'
 
-cp "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1" "$dotfilesLoc/win/legacy-profile.ps1"
+cp "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1" "$dotfilesLoc/win/legacy-profile.ps1" 2> $null
 cp "$profile" "$dotfilesLoc/win/profile.ps1"
 cp "$env:LOCALAPPDATA/nvim/init.vim" "$dotfilesLoc/editor/nvim/init.vim"
 cp "C:/Users/$username/_vimrc" "$dotfilesLoc/win/_vimrc"
