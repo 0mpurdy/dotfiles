9a9c86dfc17e6928d3ff9de286439623387a2cd3
Michael Purdy <0mpurdy@gmail.com>
2023-11-24 14:35:43 +0000

    Consolidate _vsvimrc

--- win/pull_dotfiles.ps1
+++ win/pull_dotfiles.ps1
@@ -7,5 +7,4 @@ $dotfilesLoc='T:/dev/dotfiles'
 cp "C:/Users/$username/Documents/WindowsPowerShell/profile.ps1" "$dotfilesLoc/win/legacy-profile.ps1" 2> $null
 cp "$profile" "$dotfilesLoc/win/profile.ps1"
 cp "$env:LOCALAPPDATA/nvim/init.vim" "$dotfilesLoc/editor/nvim/init.vim"
-cp "C:/Users/$username/_vimrc" "$dotfilesLoc/win/_vimrc"
-cp "C:/Users/$username/_vsvimrc" "$dotfilesLoc/win/_vsvimrc"
+cp "C:/Users/$username/_vsvimrc" "$dotfilesLoc/editor/_vsvimrc"
--- win/push_dotfiles.ps1
+++ win/push_dotfiles.ps1
@@ -6,5 +6,4 @@ New-Item -ItemType directory -Path "C:/Users/$username/Documents/WindowsPowerShe
 cp "$dotfilesLoc/win/profile.ps1" "$profile"
 New-Item -ItemType directory -Path "$env:LOCALAPPDATA/nvim" -Force
 cp "$dotfilesLoc/editor/nvim/init.vim" "$env:LOCALAPPDATA/nvim/init.vim"
-cp "$dotfilesLoc/win/_vimrc" "C:/Users/$username/_vimrc"
-cp "$dotfilesLoc/win/_vsvimrc" "C:/Users/$username/_vsvimrc"
+cp "$dotfilesLoc/editor/_vsvimrc" "C:/Users/$username/_vsvimrc"
