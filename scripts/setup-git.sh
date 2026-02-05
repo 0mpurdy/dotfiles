1677f0db9cedecb1ebc7cc5c4714484f4b5aed61
Michael Purdy <0mpurdy@gmail.com>
2026-02-05 10:48:42 +0000

    Update git config
    Obviously it's just local config and not credentials, but creds is easy
    to type, short and no tab conflict with lola unlike local

--- scripts/setup-git.sh
+++ scripts/setup-git.sh
@@ -1,5 +1,1 @@
-git config --global user.name "Michael Purdy"
-git config --global user.email "0mpurdy@gmail.com"
-git config --global --add alias.lola 'log --graph --decorate --abbrev-commit --all --date=local --pretty=format:"%C(auto)%h%d %C(blue)%an %C(green)%cd %C(red)%GG %C(reset)%s" --date=local'
-git config --global --add alias.lol 'log --graph --decorate --abbrev-commit --all --oneline'
-
+echo 'Just manually add the aliases from dotfiles/.gitconfig'
