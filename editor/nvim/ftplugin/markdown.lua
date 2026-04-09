795676cfdc17aaccc17f67c351f5586e362a3000
Michael Purdy <0mpurdy@gmail.com>
2026-04-09 13:56:51 +0100

    Add daily log command

--- editor/nvim/ftplugin/markdown.lua
+++ editor/nvim/ftplugin/markdown.lua
@@ -1,1 +1,20 @@
+local function createDailyLog()
+  vim.api.nvim_put({ '', '## ' }, 'l', true, true)
+  vim.cmd.normal(vim.api.nvim_replace_termcodes('$<leader>ido<esc>', true, false, true))
+  vim.api.nvim_put(
+    {
+      '[Project](projects/path/readme.md)',
+      '',
+      '- PR reviews',
+      '  +',
+      '',
+    },
+    'l',
+    true,
+    true)
+  vim.cmd.normal('$')
+end
+
+vim.keymap.set("n", "<leader>il", createDailyLog, {noremap=true})
+
 vim.treesitter.start()
