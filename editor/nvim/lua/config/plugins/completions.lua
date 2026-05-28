91aefa958f3b4ba49805d96a13f7da0b5bfbb103
Michael Purdy <michael.purdy@pytilia.io>
2026-05-28 09:53:25 +0100

    Update neovim autocomplete setup

--- editor/nvim/lua/config/plugins/completions.lua
+++ editor/nvim/lua/config/plugins/completions.lua
@@ -1,8 +1,5 @@
 return {
 
-  { 'hrsh7th/nvim-cmp' },
-  { 'hrsh7th/cmp-nvim-lsp' },
-
   -- Snippet support
   { 'SirVer/ultisnips' },
   {
@@ -15,6 +12,56 @@ return {
         let g:UltiSnipsJumpBackwardTrigger="<c-z>"
       ]])
     end
+  },
+
+  -- Autocomplete
+  {
+    'hrsh7th/nvim-cmp',
+
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'quangnguyen30192/cmp-nvim-ultisnips'
+    },
+
+    config = function()
+      local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
+      local cmp = require("cmp")
+
+      local next_option = cmp.mapping(
+            function(fallback)
+              cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+            end,
+            { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+          )
+          local prev_option = cmp.mapping(
+            function(fallback)
+              cmp_ultisnips_mappings.jump_backwards(fallback)
+            end,
+            { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+          )
+
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body)
+          end,
+        },
+        mapping = {
+          ["<c-cr>"] = cmp.mapping.confirm({ select = true }),
+          ["<down>"] = next_option,
+          ["<c-n>"] = next_option,
+          ["<up>"] = prev_option,
+          ["<c-e>"] = prev_option,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "ultisnips" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
   },
 
   -- Docs generation
