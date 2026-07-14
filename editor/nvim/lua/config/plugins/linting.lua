c60f21d95912e2519ed02ade27e3573127b86ec5
Michael Purdy <0mpurdy@gmail.com>
2026-07-14 10:05:41 +0100

    Use neovim lua APIs for js lint/format settings

--- editor/nvim/lua/config/plugins/linting.lua
+++ editor/nvim/lua/config/plugins/linting.lua
@@ -8,25 +8,22 @@ return {
       vim.keymap.set("n", "]e", ":ALENextWrap<cr>", {noremap=true})
       vim.keymap.set("n", "[e", ":ALEPreviousWrap<cr>", {noremap=true})
 
-      -- todo: revisit this with proper neovim syntax
-      vim.cmd([[
-        let g:ale_linters = {
-        \ 'cs': ['OmniSharp'],
-        \ 'javascript': ['eslint'],
-        \ 'typescript': ['eslint'],
-        \ 'typescriptreact': ['eslint'],
-        \}
+      vim.g.ale_linters = {
+        cs = {'OmniSharp'},
+        javascript = {'eslint'},
+        typescript = {'eslint'},
+        typescriptreact = {'eslint'},
+      }
 
-        let g:ale_fixers = {
-        \ 'javascript': ['prettier'],
-        \ 'typescript': ['prettier'],
-        \ 'typescriptreact': ['prettier'],
-        \ 'css': ['prettier'],
-        \}
+      vim.g.ale_fixers = {
+        javascript = {'prettier'},
+        typescript = {'prettier'},
+        typescriptreact = {'prettier'},
+        css = {'prettier'},
+      }
 
-        let g:ale_linters_explicit = 1
-        let g:ale_fix_on_save = 1
-      ]])
+      vim.g.ale_linters_explicit = 1
+      vim.g.ale_fix_on_save = 1
     end
   },
 
