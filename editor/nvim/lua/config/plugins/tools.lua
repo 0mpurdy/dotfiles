cb57c064fbda9b80b772103375b625d9b08ba52f
Michael Purdy <0mpurdy@gmail.com>
2026-07-20 18:51:06 +0100

    Use leader prefix for easyalign mappings

--- editor/nvim/lua/config/plugins/tools.lua
+++ editor/nvim/lua/config/plugins/tools.lua
@@ -13,9 +13,9 @@ return {
     'junegunn/vim-easy-align',
     init = function()
       -- Start interactive EasyAlign in visual mode (e.g. vipga)
-      vim.keymap.set("x", "ga", "<Plug>(EasyAlign)")
+      vim.keymap.set("x", "<leader>ga", "<Plug>(EasyAlign)")
       -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
-      vim.keymap.set("n", "ga", "<Plug>(EasyAlign)")
+      vim.keymap.set("n", "<leader>ga", "<Plug>(EasyAlign)")
     end
   },
 
